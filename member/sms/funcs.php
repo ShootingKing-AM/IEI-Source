49bf157a2a58665d7d00228293359b7d061020c2
RohithV <rohithvegesna@gmail.com>
2015-10-18 22:15:19 +0530

    Aligned code in sms/ directory

--- member/sms/funcs.php
+++ member/sms/funcs.php
@@ -1,78 +1,88 @@
-<?php if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) ob_start("ob_gzhandler"); else ob_start(); ?>
 <?php
-include('curl.php');
-$time = time();
-
-
-function go($url){
-header('Location: '.$url);
-exit;
-}
-function input($text){
-return trim(htmlspecialchars($text));
-}
-
-function output($text,$html=true){
-if($html){
-return trim(stripslashes($text));
-} else {
-return trim(htmlspecialchars(stripslashes($text)));
-}
-}
-
-function rword($length = 10,$allow="all") {
-if($allow=="all")
-        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-elseif($allow=="num")
-        $characters = '0123456789';
-elseif($allow=="en")
-        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-elseif($allow=="low")
-        $characters = 'abcdefghijklmnopqrstuvwxyz';
-elseif($allow=="up")
-        $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip'))
+    ob_start("ob_gzhandler");
 else
-$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $randomString = '';
-        for ($i = 0; $i < $length; $i++) {
-            $randomString .= $characters[rand(0, strlen($characters) - 1)];
-        }
-        return $randomString;
-    }
-function sor($html)
-{
-$html=htmlspecialchars($html);
-return $html;
-}
-
-function clean($html)
-{
-$html = str_replace(array("\r\n", "\r", "\n"), "", $html);
-return $html;
-}
-function show($text,$color="darkred")
-{
-echo '<font color="'.$color.'">'.$text.'</font><br/>';
-ob_flush();
-flush();
-}
-function fc($text,$color="darkred")
-{
-echo '<font color="'.$color.'">'.$text.'</font>';
-ob_flush();
-flush();
-}
-
-
-function err($head="Error",$text="Error")
-{
-echo '<div class="head">'.$head.'</div><div class="content">'.$text.'</div>';
-ob_flush();
-flush();
-include "foot.php";
-ob_flush();
-flush();
-die();
-}
-header('Location: success.php');
+    ob_start();
+?>
+<?php
+	include('curl.php');
+	$time = time();
+	function go($url)
+	{
+		header('Location: ' . $url);
+		exit;
+	}
+	function input($text)
+	{
+		return trim(htmlspecialchars($text));
+	}
+	function output($text, $html = true)
+	{
+		if ($html)
+		{
+			return trim(stripslashes($text));
+		}
+		else
+		{
+			return trim(htmlspecialchars(stripslashes($text)));
+		}
+	}
+	function rword($length = 10, $allow = "all")
+	{
+		if ($allow == "all")
+			$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		elseif ($allow == "num")
+			$characters = '0123456789';
+		elseif ($allow == "en")
+			$characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		elseif ($allow == "low")
+			$characters = 'abcdefghijklmnopqrstuvwxyz';
+		elseif ($allow == "up")
+			$characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		else
+			$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		$randomString = '';
+		for ($i = 0; $i < $length; $i++)
+		{
+			$randomString .= $characters[rand(0, strlen($characters) - 1)];
+		}
+		return $randomString;
+	}
+	function sor($html)
+	{
+		$html = htmlspecialchars($html);
+		return $html;
+	}
+	function clean($html)
+	{
+		$html = str_replace(array(
+			"\r\n",
+			"\r",
+			"\n"
+		), "", $html);
+		return $html;
+	}
+	function show($text, $color = "darkred")
+	{
+		echo '<font color="' . $color . '">' . $text . '</font><br/>';
+		ob_flush();
+		flush();
+	}
+	function fc($text, $color = "darkred")
+	{
+		echo '<font color="' . $color . '">' . $text . '</font>';
+		ob_flush();
+		flush();
+	}
+	function err($head = "Error", $text = "Error")
+	{
+		echo '<div class="head">' . $head . '</div><div class="content">' . $text . '</div>';
+		ob_flush();
+		flush();
+		include "foot.php";
+		ob_flush();
+		flush();
+		die();
+	}
+	header('Location: success.php');
 ?>
